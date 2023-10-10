fe3ed5d6960cb438ea98d8483d3ade9f386906f4
codewitharun <arunk4it@gmail.com>
2023-10-11 02:47:25 +0530

    nav footer 404 done

---
+++ src/app/container.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function container() {
+  return (
+    <div className="bg-white dark:bg-transparent">
+      <div className="mx-auto w-full max-w-screen-xl"></div>
+    </div>
+  );
+}
+
+export default container;
--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -26,7 +26,7 @@ function Navbar() {
   ];
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 mb-10">
+    <nav className="bg-white dark:bg-transparent fixed w-full z-20 top-0 left-0  dark:border-gray-600 mb-10">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="https://devarun.tech/"
@@ -78,7 +78,7 @@ function Navbar() {
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium  md:flex-row md:space-x-8 md:mt-0 md:border-0">
             {data.map((item) => (
               <li key={item.to}>
                 <a
@@ -86,7 +86,7 @@ function Navbar() {
                   className={
                     router === item.to
                       ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                      : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-300 md:p-0 md:dark:hover:text-blue-300 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   }
                   aria-current="page"
                 >
