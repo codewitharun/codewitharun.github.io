848c806d9f96340e66d43a874a4f1e2ee8980c07
Arun Kumar <arunk4it@gmail.com>
2024-11-21 01:53:56 +0530

    blog added

--- src/components/MultiPageRoutes.js
+++ src/components/MultiPageRoutes.js
@@ -3,6 +3,9 @@ import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Blog from "./blog/Blog";
+import BlogSection from "./blog/BlogSection";
+import BlogDetails from "./blog/BlogDetails";
 
 export default function MultiPageRoutes() {
     return (
@@ -10,6 +13,9 @@ export default function MultiPageRoutes() {
             <Route exact path={'/'} element={<Home />} />
             <Route exact path={'/about'} element={<About />} />
             <Route exact path={'/portfolio'} element={<Portfolio />} />
+            {/* <Route exact path={'/blog'} element={<Blog />} /> */}
+            <Route path="/blog" element={<BlogSection />} />
+            <Route path="/blog/:id" element={<BlogDetails />} />
         </Routes>
     )
 }
--- src/components/SinglePageRoutes.js
+++ src/components/SinglePageRoutes.js
@@ -4,10 +4,12 @@ import Portfolio from "./portfolio/Portfolio";
 import React from 'react';
 import { Box } from "@mui/material";
 
-export default function SinglePageRoutes({refs}) {
+export default function SinglePageRoutes({ refs }) {
     return (<Box mt={'3rem'}>
-        <Home innerRef={refs.refHome}/>
-        <About innerRef={refs.refAbout}/>
-        <Portfolio innerRef={refs.refPortfolio}/>
+        <Home innerRef={refs.refHome} />
+        <About innerRef={refs.refAbout} />
+        <Portfolio innerRef={refs.refPortfolio} />
+        
+
     </Box>)
 }
