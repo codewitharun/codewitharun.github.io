0e864b1252fa6580fa5c583dee17eaf819b369f4
Arun Kumar <arunk4it@gmail.com>
2024-11-25 03:04:15 +0530

    blog backend integrated

--- src/components/MultiPageRoutes.js
+++ src/components/MultiPageRoutes.js
@@ -3,14 +3,13 @@ import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Blog from "./blog/Blog";
 import BlogSection from "./blog/BlogSection";
 import BlogDetails from "./blog/BlogDetails";
 
 export default function MultiPageRoutes() {
     return (
         <Routes>
-            <Route exact path={'/'} element={<Home />} />
+            <Route exact path={'/'} element={<Home />}  />
             <Route exact path={'/about'} element={<About />} />
             <Route exact path={'/portfolio'} element={<Portfolio />} />
             {/* <Route exact path={'/blog'} element={<Blog />} /> */}
