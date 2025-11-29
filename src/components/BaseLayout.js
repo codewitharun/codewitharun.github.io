9bd6081bd3f0af45ad99f7e79757864e935737e5
Arun Singh <arunk4it@gmail.com>
2025-11-30 01:27:05 +0530

    Update footer credit from App Aura to Arun Kumar

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -49,7 +49,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                   py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
-                  <p>Created with ❤️ by <a href={'https://appaura.xyz'}>App Aura</a></p>
+                  <p>Created with ❤️ by <a href={'https://www.arun.codes'}>Arun Kumar</a></p>
                   <p>&copy; 2024</p>
                </Box>
             </Grid>
