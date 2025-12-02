1a14038a4ea8a93c0816623a39f00571b1e48cd8
Arun Singh <arunk4it@gmail.com>
2025-12-02 12:01:57 +0000

    new info added

---
+++ src/info/Info copy.js
@@ -0,0 +1,151 @@
+import self from "../img/self.png";
+import mock1 from "../img/mock1.png";
+import mock2 from "../img/mock2.png";
+import mock3 from "../img/mock3.png";
+import mock4 from "../img/mock4.png";
+import mock5 from "../img/mock5.png";
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+/* 
+This variable will change the layout of the website from multipage to single, scrollable page
+*/
+export let singlePage = false;
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+  firstName: "Arun",
+  lastName: "Kumar",
+  initials: "AK", // the example uses first and last, but feel free to use three or more if you like.
+  position: "a React Native Developer",
+  selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+  baseColor: colors[0],
+  miniBio: [
+    // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    {
+      emoji: "☕",
+      text: "fueled by coffee",
+    },
+    {
+      emoji: "🌎",
+      text: "based in the INDIA 🇮🇳",
+    },
+    {
+      emoji: "💼",
+      text: "React Native Developer @Durapid Technologies",
+    },
+    {
+      emoji: "📧",
+      text: "arun4dev@icloud.com",
+    },
+  ],
+  socials: [
+    {
+      link: "https://facebook.com/aadusdad",
+      icon: "fa fa-facebook",
+      label: "facebook",
+    },
+    {
+      link: "https://instagram.com",
+      icon: "fa fa-instagram",
+      label: "instagram",
+    },
+    {
+      link: "https://github.com/codewitharun",
+      icon: "fa fa-github",
+      label: "github",
+    },
+    {
+      link: "https://www.linkedin.com/in/arunk4it/",
+      icon: "fa fa-linkedin",
+      label: "linkedin",
+    },
+    {
+      link: "https://twitter.com/arunk4it",
+      icon: "fa fa-twitter",
+      label: "twitter",
+    },
+    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+    // Just change the links so that they lead to your social profiles.
+  ],
+  bio: "Hello! I'm Arun. I'm a software engineer at Durapid Technologies. I studied CompSci at AKTU, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+  skills: {
+    proficientWith: [
+      "react-native",
+      "javascript",
+      "react",
+      "git",
+      "github",
+      "bootstrap",
+      "html5",
+      "css3",
+      "figma",
+    ],
+    exposedTo: ["nodejs", "python", "adobe illustrator"],
+  },
+  hobbies: [
+    {
+      label: "cooking",
+      emoji: "🌶",
+    },
+    {
+      label: "reading",
+      emoji: "📖",
+    },
+
+    {
+      label: "movies",
+      emoji: "🎥",
+    },
+
+    // Same as above, change the emojis to match / relate to your hobbies or interests.
+    // You can also remove the emojis if you'd like, I just think they look cute :P
+  ],
+  portfolio: [
+    // This is where your portfolio projects will be detailed
+    {
+      title: "Coming Soon!!",
+      live: "https://appaura.xyz", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+      source: "https://github.com/codewitharun", // this should be a link to the **repository** of the project, where the code is hosted.
+      image: mock5,
+    },
+    // {
+    //     title: "Project 2",
+    //     live: "https://paytonpierce.dev",
+    //     source: "https://github.com/paytonjewell",
+    //     image: mock2
+    // },
+    // {
+    //     title: "Project 3",
+    //     live: "https://paytonpierce.dev",
+    //     source: "https://github.com/paytonjewell",
+    //     image: mock3
+    // },
+    // {
+    //     title: "Project 4",
+    //     live: "https://paytonpierce.dev",
+    //     source: "https://github.com/paytonjewell",
+    //     image: mock4
+    // },
+    // {
+    //     title: "Project 5",
+    //     live: "https://paytonpierce.dev",
+    //     source: "https://github.com/paytonjewell",
+    //     image: mock5
+    // }
+  ],
+};
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,151 +1,91 @@
 import self from "../img/self.png";
-import mock1 from "../img/mock1.png";
-import mock2 from "../img/mock2.png";
-import mock3 from "../img/mock3.png";
-import mock4 from "../img/mock4.png";
 import mock5 from "../img/mock5.png";
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-/* 
-This variable will change the layout of the website from multipage to single, scrollable page
-*/
 export let singlePage = false;
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
   firstName: "Arun",
   lastName: "Kumar",
-  initials: "AK", // the example uses first and last, but feel free to use three or more if you like.
-  position: "a React Native Developer",
-  selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-  gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+  initials: "AK",
+  position: "Software Engineer · Full-Stack & React Native Developer",
+  selfPortrait: self,
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`,
   baseColor: colors[0],
+
   miniBio: [
-    // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
-    {
-      emoji: "☕",
-      text: "fueled by coffee",
-    },
-    {
-      emoji: "🌎",
-      text: "based in the INDIA 🇮🇳",
-    },
-    {
-      emoji: "💼",
-      text: "React Native Developer @Durapid Technologies",
-    },
-    {
-      emoji: "📧",
-      text: "arunk4it@gmail.com",
-    },
-  ],
+    { emoji: "⚙️", text: "Engineer who builds, breaks, improves" },
+  { emoji: "💻", text: "React Native • Web • Backend" },
+  { emoji: "📍", text: "India 🇮🇳" },
+  { emoji: "📧", text: "arun4dev@icloud.com" },
+],
+
   socials: [
-    {
-      link: "https://facebook.com/aadusdad",
-      icon: "fa fa-facebook",
-      label: "facebook",
-    },
-    {
-      link: "https://instagram.com",
-      icon: "fa fa-instagram",
-      label: "instagram",
-    },
     {
       link: "https://github.com/codewitharun",
       icon: "fa fa-github",
-      label: "github",
+      label: "GitHub",
     },
     {
-      link: "https://www.linkedin.com/in/arunk4it/",
+      link: "https://linkedin.com/in/arunk4it",
       icon: "fa fa-linkedin",
-      label: "linkedin",
+      label: "LinkedIn",
     },
     {
-      link: "https://twitter.com/arunk4it",
-      icon: "fa fa-twitter",
-      label: "twitter",
+      link: "https://instagram.com/_arun.js",
+      icon: "fa fa-instagram",
+      label: "Instagram",
     },
-    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-    // Just change the links so that they lead to your social profiles.
   ],
-  bio: "Hello! I'm Arun. I'm a software engineer for OTS Solutions. I studied CompSci at AKTU, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+
+  bio: `Hey! I'm Arun — a software engineer who loves building scalable, user-focused applications. 
+  I work across mobile and web, specializing in React Native, Node.js, WebRTC, real-time communication systems, 
+  and performance-driven UI/UX. 
+  Outside work, I experiment with tech, explore automation ideas, and learn things that make me dangerous in interviews 😄.`,
+
   skills: {
     proficientWith: [
-      "react-native",
-      "javascript",
-      "react",
-      "git",
-      "github",
-      "bootstrap",
-      "html5",
-      "css3",
-      "figma",
+      "React Native",
+      "React",
+      "TypeScript",
+      "JavaScript",
+      "Redux Toolkit",
+      "WebRTC",
+      "MongoDB",
+      "Node.js",
+      "Express",
+      "Firebase",
+      "Git",
+      "CI/CD",
+      "React Native Web",
+      "Vision Camera",
+      "Skia",
+      "HTML5",
+      "CSS3",
+      "Figma",
     ],
-    exposedTo: ["nodejs", "python", "adobe illustrator"],
+    learning: [
+      "Swift",
+      "Kotlin",
+      "Cloud Tech (AWS/GCP)",
+      "Mobile Security",
+      "Machine Learning Filters",
+    ],
   },
+
   hobbies: [
-    {
-      label: "cooking",
-      emoji: "🌶",
-    },
-    {
-      label: "reading",
-      emoji: "📖",
-    },
-
-    {
-      label: "movies",
-      emoji: "🎥",
-    },
-
-    // Same as above, change the emojis to match / relate to your hobbies or interests.
-    // You can also remove the emojis if you'd like, I just think they look cute :P
+    { label: "Cooking", emoji: "🌶" },
+    { label: "Reading", emoji: "📖" },
+    { label: "Movies", emoji: "🎥" },
+    { label: "Tech Experiments", emoji: "⚙️" },
   ],
+
   portfolio: [
-    // This is where your portfolio projects will be detailed
     {
-      title: "Coming Soon!!",
-      live: "https://appaura.xyz", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-      source: "https://github.com/codewitharun", // this should be a link to the **repository** of the project, where the code is hosted.
+      title: "Coming Soon 🚀",
+      live: "https://appaura.xyz",
+      source: "https://github.com/codewitharun",
       image: mock5,
     },
-    // {
-    //     title: "Project 2",
-    //     live: "https://paytonpierce.dev",
-    //     source: "https://github.com/paytonjewell",
-    //     image: mock2
-    // },
-    // {
-    //     title: "Project 3",
-    //     live: "https://paytonpierce.dev",
-    //     source: "https://github.com/paytonjewell",
-    //     image: mock3
-    // },
-    // {
-    //     title: "Project 4",
-    //     live: "https://paytonpierce.dev",
-    //     source: "https://github.com/paytonjewell",
-    //     image: mock4
-    // },
-    // {
-    //     title: "Project 5",
-    //     live: "https://paytonpierce.dev",
-    //     source: "https://github.com/paytonjewell",
-    //     image: mock5
-    // }
   ],
 };
